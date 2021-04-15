de627b28bde6e543683ad354215b6cd48a2cce12
Ariel Tolay <breiks12@gmail.com>
2021-04-15 16:56:06 -0400

    Pregunta 5

--- tarea_1.js
+++ tarea_1.js
@@ -1,27 +1,18 @@
-////pregunta 4////
-function calculo(arr) {
-    var producto;
-    if (arr.length == 1) {
-        producto = arr[0];
+////pregunta 5////
+function estatuas(arr) {
+    var ordenando = arr.sort();
+    var llenar = [];
+    var a = ordenando[0];
+    var b = ordenando[ordenando.length - 1];
+    for (var i = a; i <= b; i++) {
+        llenar.push(i);
     }
-    else {
-        producto = arr[0] * arr[1];
-        for (var i = 0; i < arr.length; i++) {
-            if (i == arr.length - 1) {
-                if (arr[i] > producto) {
-                    producto = arr[i];
-                }
-            }
-            else {
-                if (arr[i] * arr[i + 1] > producto) {
-                    producto = arr[i] * arr[i + 1];
-                }
-            }
-        }
-    }
-    return producto;
+    var tam = ordenando.length - llenar.length;
+    return Math.abs(tam);
 }
-//var arr = [-1, -2]
-var arr = [3, 6, -2, -5, 7, 3];
-//var arr = [1, 0, 1, 0, 1000]
-console.log("Producto Mayor : ", calculo(arr));
+//var arr = [6, 2, 3, 8];
+//var arr = [5, 4, 6];
+var arr = [0, 3];
+// 2,3,4,5,6,7,8
+// 2,3,6,8
+console.log("Se requiere :  ", estatuas(arr), "estatuas");
