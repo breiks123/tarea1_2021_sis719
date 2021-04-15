a11fe95a621136df84b6109545a46b7e48d2ecbb
Ariel Tolay <breiks12@gmail.com>
2021-04-15 04:19:06 -0400

    Pregunta 4

--- tarea_1.js
+++ tarea_1.js
@@ -1,11 +1,27 @@
-////pregunta 3////
-function palindromo(str) {
-    var normalized = str.toLowerCase().match(/[a-z]/gi).reverse();
-    return normalized.join('') === normalized.reverse().join('');
+////pregunta 4////
+function calculo(arr) {
+    var producto;
+    if (arr.length == 1) {
+        producto = arr[0];
+    }
+    else {
+        producto = arr[0] * arr[1];
+        for (var i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                if (arr[i] > producto) {
+                    producto = arr[i];
+                }
+            }
+            else {
+                if (arr[i] * arr[i + 1] > producto) {
+                    producto = arr[i] * arr[i + 1];
+                }
+            }
+        }
+    }
+    return producto;
 }
-if (palindromo("doctor")) {
-    console.log("Es palindromo");
-}
-else {
-    console.log("No es palindromo");
-}
+//var arr = [-1, -2]
+var arr = [3, 6, -2, -5, 7, 3];
+//var arr = [1, 0, 1, 0, 1000]
+console.log("Producto Mayor : ", calculo(arr));
