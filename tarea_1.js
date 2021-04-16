12af133baa008d7dede732eb77f8115b226f1702
Ariel Tolay <breiks12@gmail.com>
2021-04-15 21:01:44 -0400

    Pregunta 10

--- tarea_1.js
+++ tarea_1.js
@@ -1,25 +1,19 @@
-////pregunta 8////
-function primo(a) {
-    if (a == 1 || a == 0) {
+////pregunta 10 ////
+function ValidarIp(str) {
+    var verdad = str.split('.');
+    if (verdad.length != 4)
         return false;
-    }
-    for (var i = 2; i < a; i++) {
-        if (a % i == 0)
+    for (var i in verdad) {
+        if (!/^\d+$/g.test(verdad[i])
+            || +verdad[i] > 255
+            || +verdad[i] < 0
+            || /^[0][0-9]{1,2}/.test(verdad[i]))
             return false;
     }
     return true;
 }
-function llenar() {
-    var arr = [];
-    for (var i = 1; i <= 10000; i++) {
-        if (primo(i) == true) {
-            arr.push(i);
-        }
-    }
-    return arr;
-}
-var limite = 6;
-var temp = llenar();
-for (var h = 0; h < limite; h++) {
-    console.log(temp[h], " ");
-}
+console.log(ValidarIp("192.168.1.255"));
+console.log(ValidarIp("255.255.255.255"));
+console.log(ValidarIp("0.0.0.0"));
+console.log(ValidarIp("30.168.1.255.1"));
+console.log(ValidarIp("127.1"));
